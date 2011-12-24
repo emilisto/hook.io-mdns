2a468e0fbdda0474376de13dda9c67944c958d5e
Emil Stenqvist <emsten@gmail.com>
2011-12-24 11:39:42 +0100

    Small fix.

--- bin/test-start.js
+++ bin/test-start.js
@@ -5,7 +5,8 @@ var hook = new mDNSHook({
 });
 
 hook.start({
-  'hook-port': 30000,
+  'hook-port': 5000,
+  // Required for hook to listen for non-local connections
   'hook-host': "0.0.0.0",
   'mdns-name': 'Bonjour Madame',
   'mdns-regtype': 'hookio'
